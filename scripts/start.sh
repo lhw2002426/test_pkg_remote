540ec7fae632a9aac91b3d25986700b073617ab5
lhw2002426 <1466397747@qq.com>
2026-04-25 09:52:48 +0800

    use new std

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,14 +1,2 @@
 #!/usr/bin/env bash
-# Start script for test_pkg_remote
-# Called by: rbnx start -p <package> -n <node>
-# Working directory: package root
-
-set -euo pipefail
-
 echo "Starting test_pkg_remote ..."
-
-# TODO: Add your start command here.
-# Examples:
-#   source /opt/ros/humble/setup.bash && ros2 launch test_pkg_remote launch.py
-#   python3 -m test_pkg_remote.main
-#   exec ./rbnx-build/test_pkg_remote
